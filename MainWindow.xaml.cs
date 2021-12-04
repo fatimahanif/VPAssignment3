f3e21ad2634a720ba25e04beefe7d391a59d3e01
fatima hanif <fatimahanif303@gmail.com>
2021-12-04 14:33:41 +0500

    commit before implementing background worker

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace Assignment3
 {
     /// <summary>
@@ -22,35 +21,114 @@ namespace Assignment3
     public partial class MainWindow : Window
     {
         public List<string> files = new List<string>();
+        BackgroundWorker backgroundWorker = new BackgroundWorker();
         public MainWindow()
         {
             InitializeComponent();
             files_listbox.ItemsSource = files;
+
+            //Background Methods
+            backgroundWorker.DoWork += backgroundWorker_DoWork;
+            backgroundWorker.ProgressChanged += backgroundWorker_ProgressChanged;
+            backgroundWorker.RunWorkerCompleted += backgroundWorker_RunWorkerCompleted;
+
+            //Background Properties
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
         }
 
+        #region Background Method 
+        // Background Worker Methods
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
         private void start_button_click(object sender, RoutedEventArgs e)
         {
             searchFiles(path_textBox.Text);
         }
 
-        private void searchFiles(string path) 
+        #region browsing directory and subdirectories
+        private void searchFiles(string path)
         {
+            //System.Threading.Thread.Sleep(100);
+
             //browsing subdirectories
             foreach (string directory in Directory.GetDirectories(path))
             {
                 searchFiles(directory);
             }
+
             //browsing directory
             foreach (string file in Directory.GetFiles(path))
             {
-                Console.WriteLine(file);
-                //files_listbox.Items.Refresh();
+                string extension = GetExtension(getFileOrFolder(file));
+                if (image_checkbox.IsChecked == true && (extension.Equals("jpg") || extension.Equals("png") || extension.Equals("gif")))
+                {
+                    files.Add($"{getFileOrFolder(file)}: {file}");
+                }
+                if (pdf_checkbox.IsChecked == true && extension.Equals("pdf"))
+                {
+                    files.Add($"{getFileOrFolder(file)}: {file}");
+                }
+                if (audio_checkbox.IsChecked == true && (extension.Equals("mp3") || extension.Equals("aac")))
+                {
+                    files.Add($"{getFileOrFolder(file)}: {file}");
+                }
+                if (video_checkbox.IsChecked == true && extension.Equals("mp4"))
+                {
+                    files.Add($"{getFileOrFolder(file)}: {file}");
+                }
+                Console.WriteLine(Path.GetExtension(path));
+                files_listbox.Items.Refresh();
                 //System.Threading.Thread.Sleep(100);
                 //files_listbox.Items.Add(file);
-                //files.Add(file);
-                //files_listbox.Items.Refresh();
+
             }
             return;
         }
+        #endregion
+
+        #region Helper Function
+        // Get the file or folder name
+        public static string getFileOrFolder(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+
+            string normalizedPath = path.Replace("/", "\\");
+            int lastIndex = normalizedPath.LastIndexOf('\\');
+            if (lastIndex <= 0)
+                return path;
+            return path.Substring(lastIndex + 1);
+        }
+
+        #region Get Extension function
+        /// <summary>
+        /// Return the Extension 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static string GetExtension(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                return string.Empty;
+            int lastIndex = fileName.LastIndexOf('.');
+            return fileName.Substring(lastIndex + 1);
+        }
+        #endregion
+
+        #endregion 
     }
 }
